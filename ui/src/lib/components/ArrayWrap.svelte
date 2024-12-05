5990d749aa2f437ec04f1860b9bd645c38df7118
Daniel Railean <daniel.railean1@gmail.com>
2024-12-05 19:32:50 +0100

    small tweaks.

--- ui/src/lib/components/ArrayWrap.svelte
+++ ui/src/lib/components/ArrayWrap.svelte
@@ -17,6 +17,7 @@
 	import Toggle from './Toggle.svelte';
 	import { writable } from 'svelte/store';
 	import { ChevronLeftOutline, ChevronRightOutline } from 'flowbite-svelte-icons';
+	import TreeWrapper from './treeWrapper.svelte';
 
 	const dispatch = createEventDispatcher();
 
@@ -248,17 +249,18 @@
 			</select>
 		</div>
 		<div class="info py-4 flex flex-row items-center space-x-4 pl-2">
-			<button class="p-2 dark:bg-stone-700" on:click={scrollPrevious}>
+			<button disabled={pageNumber == intervalsIterable[0]} class="p-2 dark:bg-stone-700 disabled:dark:bg-stone-800 disabled:bg-stone-400 disabled:text-white bg-stone-200" on:click={scrollPrevious}>
 				<ChevronLeftOutline class="size-4" />
 			</button>
 
-			{#each intervalsIterable as interval, index}
+			{#each intervalsIterable as interval}
 				{#if isVisiblePage(interval, pageNumber)}
 					<button
-						class="p-2 dark:bg-stone-700 w-10 h-10 rounded-lg"
+						class="p-2 dark:bg-stone-700 w-10 h-10 rounded-lg disabled:dark:bg-stone-800 disabled:bg-stone-400 disabled:text-white bg-stone-200"
 						on:click={() => {
 							loadPage(interval);
 						}}
+						disabled={pageNumber == interval}
 					>
 						<p>{interval}</p>
 					</button>
@@ -268,7 +270,7 @@
 				{/if}
 				<!-- content here -->
 			{/each}
-			<button class="p-2 dark:bg-stone-700" on:click={scrollNext}
+			<button disabled={pageNumber == intervalsIterable.at(-1)} class="p-2 dark:bg-stone-700 disabled:dark:bg-stone-800 disabled:bg-stone-400 disabled:text-white bg-stone-200" on:click={scrollNext}
 				><ChevronRightOutline class="size-4" /></button
 			>
 			<p class="w-36 text-center text-md">showing {arrayStart + 1} to {arrayEnd}</p>
@@ -276,7 +278,7 @@
 	</div>
 	{#if filteredData.length > 0}
 		<!-- content here -->
-		<table class="w-full">
+		<table class="w-full mb-2">
 			<thead class="text-stone-800 dark:bg-stone-800 bg-gray-200 dark:text-stone-400">
 				<tr>
 					<th><p class="pl-4 text-center">No.</p></th>
@@ -389,6 +391,9 @@
 														goto(`${base}/entity?type=${field}s&id=${item[field].id}`)}
 													title="open {field}"
 													href="{base}/entity?type={field}s&id={item[field].id}"
+													on:auxclick={() => {
+														window.open(`${base}/entity?type=${field}s&id=${item[field].id}`, '_blank');
+													}}
 												>
 													<div>
 														<p class="dark:text-blue-500 text-blue-700">{item[field].id}</p>
